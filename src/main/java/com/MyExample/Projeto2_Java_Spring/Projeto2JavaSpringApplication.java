1737174ad484038a2c76cec9b521a2a957b04ef9
Nathan Paiva <nathanpaiva00@gmail.com>
2023-08-29 16:09:04 -0300

    Project created

---
+++ src/main/java/com/MyExample/Projeto2_Java_Spring/Projeto2JavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.MyExample.Projeto2_Java_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto2JavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto2JavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyExample/Projeto2_Java_Spring/Projeto2JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyExample.Projeto2_Java_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto2JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
